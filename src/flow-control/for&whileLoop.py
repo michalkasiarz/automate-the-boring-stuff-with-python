ede4b2529afd3c8d32e7d19e9e21eba3cc8fdce7
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 15:29:44 +0100

    for & while loop for Jimmy Five Times

---
+++ src/flow-control/for&whileLoop.py
@@ -0,0 +1,14 @@
+# For loop
+
+print("My name is")
+for i in range(5):
+    print("Jimmy Five Times " + str(i))
+
+
+# while loop
+
+print("My name is")
+i = 0
+while i < 5:
+    print("Jimmy Five Times " + str(i))
+    i += 1
--- src/flow-control/forLoop.py
+++
@@ -1,5 +0,0 @@
-# For loop
-
-print("My name is")
-for i in range(5):
-    print("Jimmy Five Times " + str(i))
