76935ae24acd050c0aa123d125ce5f65e19d077d
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 16:02:59 +0100

    using pyperclip

--- src/functions/basicBuiltInFunctions.py
+++ src/functions/basicBuiltInFunctions.py
@@ -1,6 +1,14 @@
 # Basic built-in functions in Python
 
 import random
+import sys
+import pyperclip
 
 #   prints random number between 0 and 100
 print(random.randint(0, 100))
+
+pyperclip.copy("Hello world!")
+print(pyperclip.paste())
+
+sys.exit()
+
