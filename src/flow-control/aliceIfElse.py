83243834b20eba7977b115e8a7df9127691d38c7
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 14:47:26 +0100

    structure of the files corrected, another if-else script added

---
+++ src/flow-control/aliceIfElse.py
@@ -0,0 +1,13 @@
+# If-else with Alice
+
+name = "Bob"
+age = 3000
+
+if name == "Alice":
+    print("Hi Alice!")
+elif age < 12:
+    print("You are not Alice, kiddo!")
+elif age > 2000:
+    print("Unlike you, Alice is not an undead, immortal vampire!")
+elif age > 100:
+    print("You are not Alice, grannie.")
