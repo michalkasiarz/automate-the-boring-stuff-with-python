fcbd1d758294ea0ee74d4524d85771b91ae785d2
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 15:32:54 +0100

    for loop with step backwards

--- src/flow-control/for&whileLoop.py
+++ src/flow-control/for&whileLoop.py
@@ -12,3 +12,9 @@ i = 0
 while i < 5:
     print("Jimmy Five Times " + str(i))
     i += 1
+
+# for loop with range and step backwards
+
+print("My name is")
+for i in range(5, -1, -1):
+    print("Jimmy Five Times " + str(i))
