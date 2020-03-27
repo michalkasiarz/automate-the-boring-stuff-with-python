9eb99d2a4d20fd9077ef6e4c12d6c790c5ff7adb
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-27 15:09:55 +0100

    for loop and Gauss riddle

---
+++ src/flow-control/GaussRiddle.py
@@ -0,0 +1,6 @@
+# Gauss riddle
+
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
---
+++ src/flow-control/forLoop.py
@@ -0,0 +1,5 @@
+# For loop
+
+print("My name is")
+for i in range(5):
+    print("Jimmy Five Times " + str(i))
