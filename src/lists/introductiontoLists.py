676bf597c6a3f32f17358182b941f8af03a8247e
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 07:55:43 +0100

    slicing lists and assigning new values

--- src/lists/introductiontoLists.py
+++ src/lists/introductiontoLists.py
@@ -3,6 +3,36 @@
 a = ["cat", "bat", "rat", "elephant"]
 print(a)
 
+print()
+
 spam = ["cat", "bat", "rat", "elephant"]
 print(spam[0])  # prints the first item in the list - cat
 print(spam[-1])     # prints the last element in the list - elephant
+
+print()
+
+# lists of lists
+spam = [["cat", "bat"], [10, 20, 30, 40, 50], ["round", "random", "goes"]]
+
+print(spam[1][2])  # prints 30
+print(spam[0][1])   # prints bat
+print(spam[2][2])   # prints goes
+
+print()
+
+# cat goes round
+print(spam[0][0], end="     ")
+print(spam[2][2], end="     ")
+print(spam[2][0])
+
+print()
+
+spam = ["cat", "bat", "rat", "eagle", "hamster", "rabbit"]
+# slicing lists
+print(spam[2:4])    # rat, eagle
+
+#   assigning new values
+
+print(spam)
+spam[1] = "NEW VALUE, GUYS!"
+print(spam)
