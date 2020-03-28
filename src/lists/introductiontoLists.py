e867d72b2dfee4a927e29d66dc9dd3f121b10add
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 07:40:57 +0100

    introduction to lists

---
+++ src/lists/introductiontoLists.py
@@ -0,0 +1,8 @@
+# introduction to lists
+
+a = ["cat", "bat", "rat", "elephant"]
+print(a)
+
+spam = ["cat", "bat", "rat", "elephant"]
+print(spam[0])  # prints the first item in the list - cat
+print(spam[-1])     # prints the last element in the list - elephant
