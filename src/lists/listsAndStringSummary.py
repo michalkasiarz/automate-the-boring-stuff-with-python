9237e9fe2ea67f3f3c9affb7ce6a7ce979957807
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 10:12:20 +0100

    code formatting

--- src/lists/listsAndStringSummary.py
+++ src/lists/listsAndStringSummary.py
@@ -17,7 +17,7 @@ print(a)
 # a[0] = "c"  # TypeError here!
 
 spam = 42
-cheese = spam   # i.e. cheese = 42, because spam is 42 now
+cheese = spam  # i.e. cheese = 42, because spam is 42 now
 print(cheese)
 
 print()
@@ -25,17 +25,31 @@ print()
 # reference type
 spam = ["A", "B", "C", "D", "E", "F", "G", "H"]
 print(spam)
-cheese = spam   # creates another reference to a list object
+cheese = spam  # creates another reference to a list object
 cheese[1] = "NEW VALUE"
-print("Cheese: " + str(cheese))   # ['A', 'NEW VALUE', 'C', 'D', 'E', 'F', 'G', 'H']
-print("Spam: " + str(spam))     # that is also changed: # ['A', 'NEW VALUE', 'C', 'D', 'E', 'F', 'G', 'H']
+print("Cheese: " + str(cheese))  # ['A', 'NEW VALUE', 'C', 'D', 'E', 'F', 'G', 'H']
+print("Spam: " + str(spam))  # that is also changed: # ['A', 'NEW VALUE', 'C', 'D', 'E', 'F', 'G', 'H']
 
 print()
 
 # copy module
 spam = ["A", "B", "C", "D", "E", "F", "G", "H"]
 print(spam)
-cheese = copy.deepcopy(spam)    # creates new object
+cheese = copy.deepcopy(spam)  # creates new object
 cheese[1] = "NEW VALUE"
 print("Cheese: " + str(cheese))
 print("Spam: " + str(spam))
+
+# code formatting
+
+spam = ["apples",
+        "oranges",
+        "bananas",
+        "cats"]
+
+#   Four score and seven and something more and more! Remember! That is is still one line!
+print("Four score and seven" + \
+      " and something more" + \
+      " and more!" + \
+      " Remember!" + \
+      " That is is still one line!")
