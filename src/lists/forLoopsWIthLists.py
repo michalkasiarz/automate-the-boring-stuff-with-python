71779f6b1e6fe609572da672a16158d34ad209c6
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 08:18:35 +0100

    for loops with lists

---
+++ src/lists/forLoopsWIthLists.py
@@ -0,0 +1,25 @@
+# for loop with lists
+
+# printing values from a list
+for i in [0, 1, 2, 3, 4]:
+    print(i)
+
+
+print()
+
+
+# printing values from a list with a range
+for i in range(0, 5):
+    print(i)
+
+print()
+
+# range with step 2
+
+myListWithStep2 = list(range(0, 10, 2))
+print(myListWithStep2)
+
+print()
+
+for i in range(len(myListWithStep2)):
+    print("List value: " + str(myListWithStep2[i]) + ".")
