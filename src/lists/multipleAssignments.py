74b7ab5b39a485b827d633b3420c087099f7ed9a
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 08:27:33 +0100

    multiple assignments

---
+++ src/lists/multipleAssignments.py
@@ -0,0 +1,17 @@
+#   Multiple assignments training
+
+cat = ["old", "lazy", "gray"]
+cat_character = cat[1]
+cat_color = cat[2]
+cat_age = cat[0]
+
+print(cat)
+
+print()
+
+print(cat_character, cat_color, cat_age, sep=" ")
+
+# multiple assignments
+
+year, character, appearance = cat
+print(year, character, appearance, sep=" ")
