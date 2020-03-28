b53bd1a25eb0acd344ae7259da3ab0787679b2fe
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 16:21:37 +0100

    python runnable script

---
+++ src/my-python-scripts/hello.bat
@@ -0,0 +1,2 @@
+@py C:\Users\micha\Documents\PythonProjects\automate-the-boring-stuff-with-python\src\my-python-scripts\hello.py %*
+@pause
---
+++ src/my-python-scripts/hello.py
@@ -0,0 +1,6 @@
+#! python3
+import sys
+
+name = input("Hello, what is your name?\n")
+print("Hello, %s!" % name)
+print(sys.argv)
