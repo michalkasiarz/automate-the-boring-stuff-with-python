26a377cea2df8c5da9d6b1307b57f1ed1948360b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 07:18:20 +0100

    validating negative input added

--- src/handling-errors/catsExample.py
+++ src/handling-errors/catsExample.py
@@ -5,7 +5,10 @@ numCats = input()
 try:
     if int(numCats) >= 4:
         print("That is a lot of cats!")
-    else:
+
+    elif int(numCats) >= 0:
         print("That is not that many cats.")
+    else:
+        print("You've entered wrong value, man!")
 except ValueError:
     print("You did not enter a number.")
