9cd0056fedad70b6597f38e69bf84f1b43eabe1b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-28 08:32:31 +0100

    multiple assignment - swap

---
+++ src/lists/multipleAssignmentSwap.py
@@ -0,0 +1,15 @@
+# Multiple assignment - swap
+
+a = "AAA"    # a is AAA
+b = "BBB"    # b is BBB
+
+print("a is " + a)
+print("b is " + b)
+
+print()
+
+# swap
+a, b = b, a     # a is now b, i.e. a = BBB, and b = AAA
+
+print("a is " + a)      # a is BBB
+print("b is " + b)      # b is AAA
