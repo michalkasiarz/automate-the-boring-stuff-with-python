f39869c3d7a514d0f5831d606d205295aa1602b1
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-30 14:30:13 +0200

    using dot in a regex, one more example

--- src/regular-expressions/usingDotInRegex.py
+++ src/regular-expressions/usingDotInRegex.py
@@ -2,6 +2,9 @@ import re
 
 # using dot in a regex
 
-atRegex = re.compile(r".at")
+atRegex = re.compile(r".at")    # matches everything that has one char before at
 mo = atRegex.findall("The cat in the hat sat on the flat mat.")
 print(mo)   # ['cat', 'hat', 'sat', 'lat', 'mat']
+atRegex = re.compile(r".{1,2}at")    # matches everything that has one or two chars before at
+mo = atRegex.findall("The cat in the hat sat on the flat mat.")
+print(mo)   # [' cat', ' hat', ' sat', 'flat', ' mat']
