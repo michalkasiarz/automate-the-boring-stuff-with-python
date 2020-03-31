d87fb7fb9e85aa3d14309d7a95d49828ab1e1c84
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-03-31 10:56:11 +0200

    using verbose mode (working one)

--- src/regular-expressions/verboseMode.py
+++ src/regular-expressions/verboseMode.py
@@ -2,10 +2,16 @@ import re
 
 # verbose mode with re.verbose
 
-pattern = re.compile(r"""
-(\d\d\d-)|     # area code (without parentheses, with dash)
-(\(\d\d\d\))   # -or- are code with parentheses and no dash
-\d\d\d         # first 3 digits
--              # second dash
-\d\d\d\d       # last 4 digits
-\sx\d{2,4}     # extension, like x1234""", re.VERBOSE | re.IGNORECASE | re.DOTALL)
+phoneRegex = re.compile(r"""
+(\d\d\d-|       # first 3 digits and first dash
+\(\d\d\d\)\s)   # -or- first three digits in parentheses, with a space, without dash
+(\d\d\d         # next 2 digits
+-               # second dash
+\d\d\d\d)       # last 4 digits
+""", re.VERBOSE | re.IGNORECASE | re.DOTALL)
+
+text = "Here are some numbers: (123) 213-1234 or 435-345-3453."
+
+mo = phoneRegex.findall(text)
+print(mo)   # [('(123) ', '213-1234'), ('435-', '345-3453')]
+
