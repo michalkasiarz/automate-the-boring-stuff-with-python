85272152c014070be12700691b63e76a75a363ba
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-01 16:22:32 +0200

    levels of logging messages, disabling them as well

--- src/debugging/loggingModule.py
+++ src/debugging/loggingModule.py
@@ -2,13 +2,24 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
+# turning on/off logging module message
+logging.disable(logging.CRITICAL)
+
+# Log levels:
+# logging.DEBUG (the lowest)
+# logging.INFO
+# logging.WARNING
+# logging.ERROR
+# logging.CRITICAL (the highest)
+
+
 logging.debug("Start of program")
 
 
 def factorial(n):
     logging.debug("Start of factorial(%s)" % (n))
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug("i is %s, total is %s" % (i, total))
 
