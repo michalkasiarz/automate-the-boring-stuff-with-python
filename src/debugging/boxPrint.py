9dda4a7c9537e23a7e31fc3113a02a06d3036e2c
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-01 15:38:08 +0200

    saving error logs into file with traceback module

--- src/debugging/boxPrint.py
+++ src/debugging/boxPrint.py
@@ -1,9 +1,19 @@
+import traceback
+
 
 # box print
 
 def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception("Symbol needs to be a String of a length 1")
+    try:
+        if (width < 2) or (height < 2):
+            raise Exception("Width and height must be greater or equal to 2")
+    except:
+        errorFile = open("error_log.txt", "a")
+        errorFile.write(traceback.format_exc())
+        errorFile.close()
+        print("The traceback info was written into error_log.txt")
     print(symbol * width)
 
     for i in range(height - 2):
@@ -12,4 +22,4 @@ def boxPrint(symbol, width, height):
     print(symbol * width)
 
 
-boxPrint("@s", 15, 5)
+boxPrint("@", 1, 5)
