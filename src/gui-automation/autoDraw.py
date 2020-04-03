5546dd0e66e2c8978f3a6b14f74828587072807b
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-03 10:36:44 +0200

    auto-draw script for paint

---
+++ src/gui-automation/autoDraw.py
@@ -0,0 +1,17 @@
+import pyautogui
+
+# auto-drawing
+
+pyautogui.click()
+distance = 200
+while distance > 0:
+    print(distance, 0)
+    pyautogui.dragRel(distance, 0, duration=0.1)    # move right
+    distance = distance - 25
+    print(0, distance)
+    pyautogui.dragRel(0, distance, duration=0.1)    # move down
+    print(-distance, 0)
+    pyautogui.dragRel(-distance, 0, duration=0.1)   # move left
+    distance = distance - 25
+    print(0, distance)
+    pyautogui.dragRel(0, -distance, duration=0.1)   # move up
