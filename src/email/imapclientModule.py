7d0f9423afa25d5c1deff40463b8ea8395edcf3d
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-03 09:32:19 +0200

    pyzmail36 module

--- src/email/imapclientModule.py
+++ src/email/imapclientModule.py
@@ -16,3 +16,20 @@ print("Messages UIDs: + " + str(UIDs))
 
 # fetching email
 raw_message = conn.fetch([8], ['BODY[]', 'FLAGS'])
+
+# using pyzmail36 (not pyzmail!) module
+message = pyzmail.PyzMessage.factory(raw_message[8][b'BODY[]'])
+
+# printing subject of the email
+print("E-mail subject: " + message.get_subject())    # How's Friday?
+
+# printing the sender
+print("Sender: " + str(message.get_addresses("from")))
+
+# printing the receiver
+print("Receiver: " + str(message.get_addresses("to")))
+
+# printing the message
+print("".center(50, "-"))
+print(message.text_part.get_payload().decode("UTF-8"))
+print("".center(50, "-"))
