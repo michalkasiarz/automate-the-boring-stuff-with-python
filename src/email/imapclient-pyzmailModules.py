dbf9311cd8fa7292ec475f3735919d9bc31edc6a
michal.kasiarz@simple.com.pl <michal.kasiarz@post.com>
2020-04-03 09:44:12 +0200

    deleting messages & basic search keys listed

--- src/email/imapclient-pyzmailModules.py
+++ src/email/imapclient-pyzmailModules.py
@@ -8,11 +8,22 @@ conn = imapclient.IMAPClient("imap.gmail.com", ssl=True)
 conn.login("mailboxtest522@gmail.com", "xxxxxx")
 
 # selecting folder in readonly mode
-conn.select_folder("INBOX", readonly=True)
+conn.select_folder("INBOX", readonly=False)
 
 # searching emails
 UIDs = conn.search(['SINCE', '02-Apr-2020'])
 print("Messages UIDs: + " + str(UIDs))
+
+# basic search keys (imapclient)
+# ALL
+# BEFORE date
+# ON date
+# SINCE date
+# SUBJECT string
+# BODY string
+# TEXT string
+# TO string
+# FROM string
 
 # fetching email
 raw_message = conn.fetch([8], ['BODY[]', 'FLAGS'])
@@ -21,7 +32,7 @@ raw_message = conn.fetch([8], ['BODY[]', 'FLAGS'])
 message = pyzmail.PyzMessage.factory(raw_message[8][b'BODY[]'])
 
 # printing subject of the email
-print("E-mail subject: " + message.get_subject())    # How's Friday?
+print("E-mail subject: " + message.get_subject())  # How's Friday?
 
 # printing the sender
 print("Sender: " + str(message.get_addresses("from")))
@@ -33,3 +44,14 @@ print("Receiver: " + str(message.get_addresses("to")))
 print("".center(50, "-"))
 print(message.text_part.get_payload().decode("UTF-8"))
 print("".center(50, "-"))
+
+# deleting an email
+try:
+    conn.delete_messages([8])
+    print("E-mail has been deleted.")
+except Exception:
+    print("Error occured!")
+
+# searching emails again
+UIDs = conn.search(['SINCE', '02-Apr-2020'])
+print("Messages UIDs: + " + str(UIDs))
